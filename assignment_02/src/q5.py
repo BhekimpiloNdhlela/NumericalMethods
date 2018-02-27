f7db1af31e2935be0faca3c5293e9c0c6dc1758b
Bhekimpilo <18998712@sun.ac.za>
2018-02-27 19:49:30 +0200

    add Dr. Hale's bits

--- assignment_02/src/q5.py
+++ assignment_02/src/q5.py
@@ -3,7 +3,7 @@ from numpy import (array, zeros, exp, linalg, negative)
 from math import (cos, sin)
 
 def newton_method_sys(fxy, j_0, j_1, debug=True):
-    xn = zeros((2, 8))      #store itteration results for x^[n + 1]
+    xn = zeros((2, 9))      #store itteration results for x^[n + 1]
     jx = zeros((2, 2))      #store currant itteration jacobian inverse
     fx = zeros((2, 1))      #store the results of the f(x^[n]) for a particular itteration
 
@@ -15,14 +15,15 @@ def newton_method_sys(fxy, j_0, j_1, debug=True):
 
         fx[0][0], fx[1][0] = fxy(xn[0][i], xn[1][i])
         
-        sx = linalg.lstsq(negative(jx), fx)[3]
+        # sx = linalg.lstsq(negative(jx), fx)[3]
+	sx = linalg.solve(negative(jx), fx)
         
         xn[0][i + 1] = xn[0][i] + sx[0]
         xn[1][i + 1] = xn[1][i] + sx[1]
 
     if debug is True:
-        print "\n", "np.linalg.lstsq(jx, Fx)[0] = \n", linalg.lstsq(jx, fx)[0], "\n"
-        print "\n", "np.linalg.lstsq(jx, Fx)[3] = \n", linalg.lstsq(jx, fx)[3], "\n"
+        # print "\n", "np.linalg.lstsq(jx, Fx)[0] = \n", linalg.lstsq(jx, fx)[0], "\n"
+        # print "\n", "np.linalg.lstsq(jx, Fx)[3] = \n", linalg.lstsq(jx, fx)[3], "\n"
         print "Fx = \n", fx, "\n"
         print "jx = \n", jx, "\n"
         print "xn = \n", xn, "\n"
