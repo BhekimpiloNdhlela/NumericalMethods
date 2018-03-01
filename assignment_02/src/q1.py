a0cc6867a92bf35daf348e14c0c2c4f7fc28359c
BhekimpiloNdhlela <18998712@sun.ac.za>
2018-03-01 20:41:34 +0200

    debug question 1

--- assignment_02/src/q1.py
+++ assignment_02/src/q1.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 def fpi(f, x, k):
     for i in xrange(k):
+        print x
         x = f(x)
     return x
 
 if __name__ == "__main__":
-    x, k = 1, 1000
+    x, k = 1, 10
     f = lambda x : (0.5 * x) + (1 / x)
     g = lambda y : ((2. * x) / 3.) + (2. / (3. * x))
     h = lambda z : (0.75 * x) + (0.5 * x)
-
+    print "The itterations for F(x):"
     print "F(x) = ", fpi(f, x, k)
-    print "G(x) = ", fpi(g, x, k)
-    print "H(x) = ", fpi(h, x, k)
+    print "The itterations for F(x):"
+    #print "G(x) = ", fpi(g, x, k)
+    print "The itterations for F(x):"
+    #print "H(x) = ", fpi(h, x, k)
 else:
     import sys
     sys.exit("please run as client...")
