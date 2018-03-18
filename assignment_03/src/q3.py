319e4bebcd792ae03e2c5e6906f3e06931a02f26
Bhekimpilo <18998712@sun.ac.za>
2018-03-18 18:11:36 +0200

    add question 3d code

--- assignment_03/src/q3.py
+++ assignment_03/src/q3.py
@@ -7,17 +7,9 @@ task    : Computer Assignment 03 question 3 (a. to e.)
 since   : Friday-09-03-2018
 """
 
-def questiona(debug=True):
-    """
-
-    write down the documentation of this question here
-
-    """
-    N = 4  #4 points from the chebyshev points
-    x = array([0.0, 0.0, 0.0, 0.0])
-    x.shape = (4, 1)
+def question_a(N=4, debug=True):
+    x = zeros(N)
     x = [cos(((2 * k - 1) * pi)/(2 * N)) for k in xrange(1, N + 1)]
-
     # defining the chebyshev Vandermonde matrix where V = 4x4 Matrix
     V = C.chebvander(x, N - 1).T
 
@@ -25,14 +17,13 @@ def questiona(debug=True):
         print "DEBUG MODE: ON [Question 3 (a.)]:"
         print "vector x   = ", x
         print "\nchebyshev Vandermonde matrix where V:      \n", V
-
     return x, V
 
-def questionb(x, V, debug=True):
+def question_b(x, V, debug=True):
     # finding the e^xi for the x elements
     exp_x = [exp(i) for i in x]
     V_inv = inv(V)
-    c = matmul(exp_x, V_inv)
+    c = matmul(V_inv, exp_x)
 
     if debug is True:
         print "\nDEBUG MODE: ON [Question 3 (b.)]:"
@@ -40,22 +31,30 @@ def questionb(x, V, debug=True):
         print "\n(chebyshev Vandermonde matrix)^-I where V :\n", V_inv
         print "\nVc=exp(x)=> c=inv(V)exp(c) <=> inv(V)V=I  :\n", c
 
-def questionc(debug=True):
-    """
-        Use the inequality on slide 10 of Lecture 13 to
-        estimate how many Chebyshev points, x 1 , . . . , x n ,
-        will be necessary to approximate the function
-        exp(x) to an accuracy of 10e−10 on the interval [0, 3].
-    """
+def question_c(debug=True):
+    pass
+
+def question_d(N=5, debug=True):
+    # N chebyshev points
+    chebyshev_points = zeros(N)
+    chebyshev_points = [cos(((2 * k - 1) * pi)/(2 * N)) for k in xrange(1, N + 1)]
+
+    if debug is True:
+        print "DEBUG MODE : ON [Question 3 d.)]"
+        print "The n Chebyshev Point where n = ", N, "are:\n", chebyshev_points
+
+def question_e(debug=True):
     pass
 
 if __name__ == "__main__":
     from math import (cos, pi, exp)
-    from numpy import (array, shape, transpose, matmul)
+    from numpy import (array, shape, transpose, matmul, zeros)
     from numpy.polynomial import chebyshev as C
     from numpy.linalg import inv
-    x, V = questiona()
-    questionb(x, V)
+    x, V = question_a(debug=False)
+    question_b(x, V, debug=False)
+    question_d(debug=False)
+    question_e()
 else:
     import sys
     sys.exit("Run the library as the client.")
