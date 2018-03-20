9f2e9dedc196c5134ecab520270a120bfce5f834
BhekimpiloNdhlela <18998712@sun.ac.za>
2018-03-20 14:46:02 +0200

    plot the functions to see whats going on

--- assignment_03/src/q1.py
+++ assignment_03/src/q1.py
@@ -9,6 +9,7 @@ since   : Friday-09-03-2018
 def question_a(debug=True):
     global J
     J = [bessel_function(i) for i in xrange(0, 4)]
+
     if debug is True:
         print "Debug Mode : ON  \t Question 1 (a.)"
         print "i", "\t", "Jv(1)"
@@ -17,8 +18,7 @@ def question_a(debug=True):
     else:
         print "Debug Mode : OFF \t Question 1 (a.)"
 
-def bessel_function(v, x=1):
-    j = 0
+def bessel_function(v, x=1, j=0):
     b = lambda k : pow(-1, k) * pow(float(x)/2, v + (2 * k)) \
                    / (sm.factorial(k) * sm.factorial(v + k))
     for k in xrange(0, 4):
@@ -34,12 +34,22 @@ def question_b(debug=True):
     P = [num(x[i]) / den(x[i]) for i in xrange(0, 4)]
 
     # plot p(v) and Jv(1) on the same system
-    plt.plot(x, J, x, P) # plotting t, b separately
+    func1, = plt.plot(x, J, label="J1(1)", linestyle='-')
+    func2, = plt.plot(x, P, label="P(v)", linestyle='-')
+    plt.title('Jx(1) and P(x) Function Tittle Should come here')
+    plt.ylabel('Jx(1) and P(x)')
+    plt.xlabel('x')
+    # include legend
+    first_legend = plt.legend(handles=[func1], loc=1)
+    ax = plt.gca().add_artist(first_legend)
+    plt.legend(handles=[func2], loc=4)
     plt.show()
-
     # plot the error function Jv(1) - p(v)
     err = [jv - pv for jv, pv in zip(J, P)]
-    plt.plot(x, err)
+    plt.plot(x, err, 'r-')
+    plt.title('Error Functioin Tittle Should come here')
+    plt.ylabel('Error: Jx(1) - P(x)')
+    plt.xlabel('x')
     plt.show()
 
     if debug is True:
@@ -50,7 +60,7 @@ def question_b(debug=True):
         print "err = Jv(1) - P(v) = ", err
     else:
         print "Debug Mode : OFF  \t Question 1 (b.)"
-
+"""
 def question_c(M4=3.0, debug=True):
     if debug is True:
         print "Debug Mode : ON  \t Question 1 (c.)"
@@ -62,6 +72,7 @@ def question_d(debug=True):
         print "Debug Mode : ON  \t Question 1 (d.)"
     else:
         print "Debug Mode : OFF \t Question 1 (d.)"
+"""
 
 if __name__ == "__main__":
     J = [.0, .0, .0, .0]
@@ -73,6 +84,7 @@ if __name__ == "__main__":
     question_a()
     question_b()
     #question_c()
+    #question_d()
 
 else:
     import sys
