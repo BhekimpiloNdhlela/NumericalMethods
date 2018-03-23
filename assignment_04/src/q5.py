b8440dcad5d3364853b1fef0d880102b0acb3ce4
Bhekimpilo <18998712@sun.ac.za>
2018-03-23 20:38:01 +0200

    finish question 5

--- assignment_04/src/q5.py
+++ assignment_04/src/q5.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-
 def question_a(n=8.0, debug=True):
-    f = array([exp(x) for x in linspace(0, 1, 8)])
+    f = array([exp(x) for x in linspace(0.0, 1.0, 8.0)])
     h = (1.0 / (n - 1.0))
-    A = matrix([[1,-2, 1, 0, 0, 0, 0, 0],[0, 1,-2, 1, 0, 0, 0, 0],\
-                [0, 0, 1,-2, 1, 0, 0, 0],[0, 0, 0, 1,-2, 1, 0, 0],\
-                [0, 0, 0, 0, 1,-2, 1, 0],[0, 0, 0, 0, 0, 1,-2, 1]])
+    A = matrix([[1,-2, 1, 0, 0, 0, 0, 0],\
+                [0, 1,-2, 1, 0, 0, 0, 0],\
+                [0, 0, 1,-2, 1, 0, 0, 0],\
+                [0, 0, 0, 1,-2, 1, 0, 0],\
+                [0, 0, 0, 0, 1,-2, 1, 0],\
+                [0, 0, 0, 0, 0, 1,-2, 1]])
     f_ = (1. / h**2.) * matmul(A, f).T
 
     if debug is True:
@@ -13,10 +15,8 @@ def question_a(n=8.0, debug=True):
         print "f\"(x) = ",
         for i in f_:
             print i,
-
+            
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    from math import pow
     from numpy import (linspace, exp, array, shape, matrix, matmul)
     question_a()
 else:
