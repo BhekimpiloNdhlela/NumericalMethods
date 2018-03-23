d907f6689984969fdd071bdda2f2b6ce578f7ace
Bhekimpilo <18998712@sun.ac.za>
2018-03-23 16:15:24 +0200

    finish question 5 and first and first push to ass 04

---
+++ assignment_04/src/q1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+def question_
+if __name__ == "__main__":
+
+else:
+    from sys import exit
+    exit("USAGE: python q1.py")
---
+++ assignment_04/src/q5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+def question_a(n=8.0, debug=True):
+    f = array([exp(x) for x in linspace(0, 1, 8)])
+    h = (1.0 / (n - 1.0))
+    A = matrix([[1,-2, 1, 0, 0, 0, 0, 0],[0, 1,-2, 1, 0, 0, 0, 0],\
+                [0, 0, 1,-2, 1, 0, 0, 0],[0, 0, 0, 1,-2, 1, 0, 0],\
+                [0, 0, 0, 0, 1,-2, 1, 0],[0, 0, 0, 0, 0, 1,-2, 1]])
+    f_ = (1. / h**2.) * matmul(A, f).T
+
+    if debug is True:
+        print "DEBUG MODE: [ON] \t [QUESTION 5 A.)]"
+        print "f\"(x) = ",
+        for i in f_:
+            print i,
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from math import pow
+    from numpy import (linspace, exp, array, shape, matrix, matmul)
+    question_a()
+else:
+    from sys import exit
+    exit("USAGE: python q5.py")
