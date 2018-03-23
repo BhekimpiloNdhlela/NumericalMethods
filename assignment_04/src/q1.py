7cdf19d73690f241cc6e39e40955f839a3e48746
Bhekimpilo <18998712@sun.ac.za>
2018-03-23 21:25:43 +0200

    add src code header to q1.py and q2.py

--- assignment_04/src/q1.py
+++ assignment_04/src/q1.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python
+"""
+author  : Bhekimpilo Ndhlela
+author  : 18998712
+module  : Applied Mathematics(Numerical Methods) TW324
+task    : computer assignment 04 question 1 (a. to b.)
+since   : Friday-23-03-2018
+"""
+def question_a(debug=True):
+    # need to add the error term to the anonymous function (c_ddx)
+    c_ddx   = lambda x, h : ((-fx(x+2*h)+8*f(x+h)-8*f(x-h) + f(x-2*h))/(12*h))
+    e_ddx   = lambda x    : -cos(x) / (sqrt(1 - 2 * sin(x)))
+    fx      = lambda x    : sqrt(1 - 2 * sin(x))
+    abs_err = lambda xc, x: abs(xc - x)
 
-def question_
+def question_b(debug=True):
+    pass
+
 if __name__ == "__main__":
+    from numpy import abs, cos, sin, sqrt
+    question_a()
 
 else:
     from sys import exit
--- assignment_04/src/q5.py
+++ assignment_04/src/q5.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
+"""
+author  : Bhekimpilo Ndhlela
+author  : 18998712
+module  : Applied Mathematics(Numerical Methods) TW324
+task    : computer assignment 04 question 5 (a.)
+since   : Friday-23-03-2018
+"""
 def question_a(n=8.0, debug=True):
-    f = array([exp(x) for x in linspace(0.0, 1.0, 8.0)])
     h = (1.0 / (n - 1.0))
+    f = array([exp(x) for x in linspace(0.0, 1.0, 8.0)])
     A = matrix([[1,-2, 1, 0, 0, 0, 0, 0],\
                 [0, 1,-2, 1, 0, 0, 0, 0],\
                 [0, 0, 1,-2, 1, 0, 0, 0],\
@@ -15,7 +22,7 @@ def question_a(n=8.0, debug=True):
         print "f\"(x) = ",
         for i in f_:
             print i,
-            
+
 if __name__ == "__main__":
     from numpy import (linspace, exp, array, shape, matrix, matmul)
     question_a()
