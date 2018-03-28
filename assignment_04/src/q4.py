c1b801e3cb27f158bc9dd8a6f9df300cbc4ac6f9
Bhekimpilo <18998712@sun.ac.za>
2018-03-28 16:14:41 +0200

    finish code for question4 might update after conslutation

--- assignment_04/src/q4.py
+++ assignment_04/src/q4.py
@@ -6,24 +6,33 @@ module  : Applied Mathematics(Numerical Methods) TW324
 task    : computer assignment 04 question 4
 since   : Friday-23-03-2018
 """
-def trapezium(abs_err, exact_I, debug=True):
-    h = [1.0, 0.1, 0.01]
-    abs_err_t = [0, 0, 0]
+def trapezium(exact_I, H, x0=0.0, debug=True):
+    apprx_I = array([h/2.0 * (exp(x0) + exp(h)) for h in H])
+    abs_err = array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
+
     if debug is True:
         print "DEBUG MODE: [ON] [Question 4 trapezium method]"
-        return abs_err_t
+        print "Trapezium: |xc - x| = ", abs_err, "\n"
+    return abs_err
 
-def midpoint(abs_err, exact_I, debug=True):
-    abs_err_m = [0, 0, 0]
+def midpoint(exact_I, H, x0=0.0, debug=True):
+    W = array([x0 + (h / 2.0) for h in H])
+    apprx_I = array([h * exp(w) for h, w in zip(H, W)])
+    abs_err = array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
+
     if debug is True:
         print "DEBUG MODE: [ON] [Question 4 Midpoint Method]"
-    return abs_err_m
+        print "Midpoint: |xc - x| = ", abs_err, "\n"
+    return abs_err
 
-def simpson(abs_err, exact_I, debug=True):
-    abs_err_s = [0, 0, 0]
+def simpson(exact_I, H, x0=0.0, debug=True):
+    apprx_I = array([h/3 * (exp(x0) + (4*exp(h/2.0)) + exp(h)) for h in H])
+    abs_err = array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
+
     if debug is True:
         print "DEBUG MODE: [ON] [Question 4 Simpson's method]"
-    return abs_err_s
+        print "Simson: |xc - x| = ", abs_err, "\n"
+    return abs_err
 
 def plot_abs_errs(abs_err_t, abs_err_m, abs_err_s):
     #loglog plot to display the error as function of the step size
@@ -34,21 +43,23 @@ def plot_abs_errs(abs_err_t, abs_err_m, abs_err_s):
     plt.xscale('log')
     plt.plot([1, 0.1, 0.01], abs_err_t, "k-", label="Trapezium")
     plt.plot([1, 0.1, 0.01], abs_err_m, "r-", label="Midpoint")
-    plt.plot([1, 0.1, 0.01], abs_err_t, "g-", label="Simpson")
+    plt.plot([1, 0.1, 0.01], abs_err_s, "g-", label="Simpson")
     plt.legend(bbox_to_anchor=(.65, .9))
     plt.show()
 
 if __name__ == "__main__":
-    from numpy import (exp, abs)
+    from numpy import (exp, abs, array)
     import matplotlib.pyplot as plt
 
-    exact_I  = lambda h : exp(h) - 1
-    abs_err  = lambda xc, x : abs(xc - x)
+    H         = array([1.0, 0.1, 0.01])
+    exact_I   = array([exp(h) - 1 for h in H])
 
-    abs_err_t = trapezium(abs_err, exact_I)
-    abs_err_m = midpoint(abs_err, exact_I)
-    abs_err_s = simpson(abs_err, exact_I)
+    abs_err_t = trapezium(exact_I, H)
+    abs_err_m = midpoint(exact_I, H)
+    abs_err_s = simpson(exact_I, H)
+
     plot_abs_errs(abs_err_t, abs_err_m, abs_err_s)
+
 else:
     from sys import exit
     exit("USAGE: python q5.py")
