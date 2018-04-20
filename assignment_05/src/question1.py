67573a55bf9f732b342e6ad790703b63150a1f7d
Bhekimpilo <18998712@sun.ac.za>
2018-04-20 15:47:19 +0200

    add code for question 3 from the course website

--- assignment_05/src/question1.py
+++ assignment_05/src/question1.py
@@ -6,26 +6,15 @@ module  : Applied Mathematics(Numerical Methods) TW324
 task    : computer assignment 05 question 1
 since   : Friday-27-04-2018
 """
+
+"""
 def composite_trapezium(exact_I, H, a=0.0, b=1.0):
     # --------------------------------------
     H = array([(b - a) / (2.0 * m) for m in M])
 
     # --------------------------------------
-
-
     apprx_I = array([h/2. * (exp(x0) + exp(h)) for h in H])
     # return absolute Errors for composite_trapezium
-    return array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
-
-def composite_midpoint(exact_I, M, a=0.0, b=1.0):
-    # --------------------------------------
-    H = array([(b - a) / m for m in M])
-    W = array([a + (h / 2.0) for h in H])
-
-    sigma = lambda m, w : sum(exp(w) for w in W)
-    apprx_I = array([h * sigma(m, w) for (h, m) in zip(H, M, W)])
-    # --------------------------------------
-    # return absolute Errors for composite_midpoint
     return array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
 
 def composite_simpson(exact_I, H, a=0.0, b=1.0):
@@ -36,24 +25,48 @@ def composite_simpson(exact_I, H, a=0.0, b=1.0):
     apprx_I = array([h/6.*(exp(x0)+(4.*exp(h/2.))+exp(h)) for h in H])
     # return absolute Errors for composite_simpson
     return array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
+"""
 
-def debug(abs_err_ct, abs_err_cm, abs_err_cs):
-    print "DEBUG MODE: [ON] [Question 1 Simpson's method]"
-    print "SIMPSONS METHOD\t\tMIDPOINT METHOD\t\tTRAPEZIUM METHOD"
-    for s, m, t in zip(abs_err_cs, abs_err_cm, abs_err_ct):
-        print "{:.20f}".format(s),"{:.20f}".format(m),"{:.20f}".format(t)
+def debug(abs_err_s, abs_err_m, abs_err_t, debug=True):
+    if debug is True:
+        print "DEBUG MODE: [ON] [Question 4 Simpson's method]"
+        print "SIMPSONS METHOD\t\tMIDPOINT METHOD\t\tTRAPEZIUM METHOD"
+        for s, m, t in zip(abs_err_s, abs_err_m, abs_err_t):
+            print "{:.20f}".format(s),"{:.20f}".format(m),"{:.20f}".format(t)
+    else:
+        print "DEBUG MODE: [OFF] [Question 4 Simpson's method]"
 
-def plot_abs_errs(abs_err_ct, abs_err_cm, abs_err_cs):
+def plot_abs_errs(abs_err_t, abs_err_m, abs_err_s):
     #loglog plot to display the error as function of the step size
     plt.title("|xc-x| of: The Midpoint, Simpson & Trapezium Methods against h")
-    plt.ylabel("Composite Midpoint vs Composite Simpson vs Composite Trapezium")
+    plt.ylabel("Midpoint vs Simpson vs Trapezium")
     plt.xlabel("h")
     plt.yscale('log')
     plt.xscale('log')
-    plt.plot([1., .1, .01], abs_err_ct, "k-", label="Composite Trapezium")
-    plt.plot([1., .1, .01], abs_err_cm, "r-", label="Composite Midpoint")
-    plt.plot([1., .1, .01], abs_err_cs, "g-", label="Composite Simpson")
+    plt.plot([1., .1, .01], abs_err_t, "k-", label="Trapezium")
+    plt.plot([1., .1, .01], abs_err_m, "r-", label="Midpoint")
+    plt.plot([1., .1, .01], abs_err_s, "g-", label="Simpson")
     plt.legend(bbox_to_anchor=(.65, .9))
+    plt.show()
+
+def composite_midpoint(M, a=0.0, b=1.0):
+    H = array([(b - a) / m for m in M])
+
+    W = array([a + (h / 2.0) for h in H])
+
+    S = lambda m: sum([exp(mi) for mi in xrange(1, int(m))])
+
+    apprx_I = [h * S(m) for (h, m) in zip(H, M)]
+
+
+
+    abs_error = array([abs(aI - (exp(1) - 1)) for aI in apprx_I])
+
+    print "Abs Error:"
+    print abs_error
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.plot(linspace(0.0, 115.0, 10), abs_error, "k--")
     plt.show()
 
 if __name__ == "__main__":
@@ -61,13 +74,13 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     from scipy import integrate
 
-    #H         = array([1., .1, .01])
-    #exact_I   = array([exp(h) - 1 for h in H])
+
 
     M = linspace(11.0, 101.0, 10)
+    composite_midpoint(M)
 
     #abs_err_ct = composite_trapezium(exact_I, H)
-    #abs_err_cm = composite_midpoint(exact_I, H)
+    #abs_err_cm = composite_midpoint(M)
     #abs_err_cs = composite_simpson(exact_I, H)
 
     #debug(abs_err_ct, abs_err_cm, abs_err_cs)
--- assignment_05/src/question3.py
+++ assignment_05/src/question3.py
@@ -7,11 +7,26 @@ task    : computer assignment 05 question 1
 since   : Friday-27-04-2018
 """
 
-def question_a(debug=True):
-    if debug == True:
-        print("Debug Mode: [ON] Question 3(a)")
-    else:
-        print("Debug Mode: [OFF] Question 3(a)")
+def romberg(f,a,b,n):
+    # this code block or the function romberg was aquired from the course website
+    """
+    def myf(x):
+    	return sin(1.7*x)-x**2.5
+    set_printoptions(precision=5)
+    print romberg(myf,1,3,5)
+    """
+
+    h=(b-a)/2.**linspace(0,n-1,n)
+    r = zeros((n,n))
+    r[0,0]=(b-a)*(f(a)+f(b))/2.
+    for j in xrange(2,n+1):
+        subtotal = 0.
+        for i in xrange(1,2**(j-2)+1):
+            subtotal = subtotal + f(a+(2*i-1)*h[j-1])
+        r[j-1,0] = r[j-2,0]/2. + h[j-1]*subtotal
+        for k in xrange(2,j+1):
+            r[j-1,k-1] = (4**(k-1)*r[j-1,k-2]-r[j-2,k-2])/(4.**(k-1)-1)
+    return r
 
 def question_b(debug=True):
     if debug == True:
