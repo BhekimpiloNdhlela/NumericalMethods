8126eb21c38b9c1134ec3e6f66b62f405f933c0d
BhekimpiloNdhlela <18998712@sun.ac.za>
2018-04-20 13:13:44 +0200

    add update code for q1

--- assignment_05/src/question1.py
+++ assignment_05/src/question1.py
@@ -6,30 +6,42 @@ module  : Applied Mathematics(Numerical Methods) TW324
 task    : computer assignment 05 question 1
 since   : Friday-27-04-2018
 """
-def composite_trapezium(exact_I, H, x0=0., debug=True):
+def composite_trapezium(exact_I, H, a=0.0, b=1.0):
+    # --------------------------------------
+    H = array([(b - a) / (2.0 * m) for m in M])
+
+    # --------------------------------------
+
+
     apprx_I = array([h/2. * (exp(x0) + exp(h)) for h in H])
     # return absolute Errors for composite_trapezium
     return array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
 
-def composite_midpoint(exact_I, H, x0=0., debug=True):
-    W = array([x0 + (h / 2.0) for h in H])
-    apprx_I = array([h * exp(w) for h, w in zip(H, W)])
+def composite_midpoint(exact_I, M, a=0.0, b=1.0):
+    # --------------------------------------
+    H = array([(b - a) / m for m in M])
+    W = array([a + (h / 2.0) for h in H])
+
+    sigma = lambda m, w : sum(exp(w) for w in W)
+    apprx_I = array([h * sigma(m, w) for (h, m) in zip(H, M, W)])
+    # --------------------------------------
     # return absolute Errors for composite_midpoint
     return array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
 
-def composite_simpson(exact_I, H, x0=0., debug=True):
+def composite_simpson(exact_I, H, a=0.0, b=1.0):
+    # --------------------------------------
+    H = array([(b - a) / m for m in M])
+    # --------------------------------------
+
     apprx_I = array([h/6.*(exp(x0)+(4.*exp(h/2.))+exp(h)) for h in H])
     # return absolute Errors for composite_simpson
     return array([abs(apI - exI) for apI, exI in zip(apprx_I, exact_I)])
 
-def debug(abs_err_ct, abs_err_cm, abs_err_cs, debug=True):
-    if debug is True:
-        print "DEBUG MODE: [ON] [Question 1 Simpson's method]"
-        print "SIMPSONS METHOD\t\tMIDPOINT METHOD\t\tTRAPEZIUM METHOD"
-        for s, m, t in zip(abs_err_cs, abs_err_cm, abs_err_ct):
-            print "{:.20f}".format(s),"{:.20f}".format(m),"{:.20f}".format(t)
-    else:
-        print "DEBUG MODE: [OFF] [Question 1]"
+def debug(abs_err_ct, abs_err_cm, abs_err_cs):
+    print "DEBUG MODE: [ON] [Question 1 Simpson's method]"
+    print "SIMPSONS METHOD\t\tMIDPOINT METHOD\t\tTRAPEZIUM METHOD"
+    for s, m, t in zip(abs_err_cs, abs_err_cm, abs_err_ct):
+        print "{:.20f}".format(s),"{:.20f}".format(m),"{:.20f}".format(t)
 
 def plot_abs_errs(abs_err_ct, abs_err_cm, abs_err_cs):
     #loglog plot to display the error as function of the step size
@@ -45,19 +57,21 @@ def plot_abs_errs(abs_err_ct, abs_err_cm, abs_err_cs):
     plt.show()
 
 if __name__ == "__main__":
-    from numpy import (exp, abs, array)
+    from numpy import (exp, abs, array, linspace)
     import matplotlib.pyplot as plt
     from scipy import integrate
 
-    H         = array([1., .1, .01])
-    exact_I   = array([exp(h) - 1 for h in H])
+    #H         = array([1., .1, .01])
+    #exact_I   = array([exp(h) - 1 for h in H])
 
-    abs_err_ct = composite_trapezium(exact_I, H)
-    abs_err_cm = composite_midpoint(exact_I, H)
-    abs_err_cs = composite_simpson(exact_I, H)
+    M = linspace(11.0, 101.0, 10)
 
-    debug(abs_err_ct, abs_err_cm, abs_err_cs)
-    plot_abs_errs(abs_err_ct, abs_err_cm, abs_err_cs)
+    #abs_err_ct = composite_trapezium(exact_I, H)
+    #abs_err_cm = composite_midpoint(exact_I, H)
+    #abs_err_cs = composite_simpson(exact_I, H)
+
+    #debug(abs_err_ct, abs_err_cm, abs_err_cs)
+    #plot_abs_errs(abs_err_ct, abs_err_cm, abs_err_cs)
 
 else:
     from sys import exit
--- assignment_05/src/question3.py
+++ assignment_05/src/question3.py
@@ -7,7 +7,28 @@ task    : computer assignment 05 question 1
 since   : Friday-27-04-2018
 """
 
+def question_a(debug=True):
+    if debug == True:
+        print("Debug Mode: [ON] Question 3(a)")
+    else:
+        print("Debug Mode: [OFF] Question 3(a)")
+
+def question_b(debug=True):
+    if debug == True:
+        print("Debug Mode: [ON] Question 3(b)")
+    else:
+        print("Debug Mode: [OFF] Question 3(b)")
+
+def question_c(debug=True):
+    if debug == True:
+        print("Debug Mode: [ON] Question 3(c)")
+    else:
+        print("Debug Mode: [OFF] Question 3(c)")
+
 if __name__ == "__main__":
+    question_a()
+    question_b()
+    question_c()
 
 else:
     from sys import exit
