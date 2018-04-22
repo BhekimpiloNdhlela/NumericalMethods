8d62c9689985cf4452fa1a07d3a6588debb24b9b
Bhekimpilo <18998712@sun.ac.za>
2018-04-22 21:08:48 +0200

    start with question 5 and 6

--- assignment_05/src/question5.py
+++ assignment_05/src/question5.py
@@ -7,7 +7,19 @@ task    : computer assignment 05 question 1
 since   : Friday-27-04-2018
 """
 
+def gauss_legendre(debug=True):
+    pass
+
+def gauss_chebyshev(debug=True):
+    pass
+
 if __name__ == "__main__":
+    from scipy import (integrate, special)
+    from math import (sqrt, exp)
+
+    I = lambda x : exp(x) / sqrt(1 - x**2)
+    exact_I = integrate.quad(I, -1, 1)[0]
+    print exact_I
 
 else:
     from sys import exit
--- assignment_05/src/question6.py
+++ assignment_05/src/question6.py
@@ -7,16 +7,24 @@ task    : computer assignment 05 question 1
 since   : Friday-27-04-2018
 """
 
-# quadratic: dv/dt = g - c2v
-# if v(0) = 0 ->> v = sqrt(gc2)tanh(tsqrt(gc2))
+def question_a(n, G=9.81, C2=1.0/1000.0, t=10.0, debug=True):
+    time = linspace(0.0, 10.0, n)
+    h_size = time[1] - time[0]
+    y = zeros(n, dtype=float)
+    # consult Dr. Hale regarding the y(0) = ?
+    # for progress sake i will assume y(0) = 0
+    f_exact = lambda t : sqrt(G/C2) * tanh(t * sqrt(G*C2))
+    f_dx_dy = lambda t : G - C2 * (t**2)
+    yt10 = f_exact(10)
+    y = [y[i-1] + h_size * f_dx_dy(t) for i, t in enumerate(time, start=1)]
 
-def question_a(N, G=9.81, C2=1.0/1000.0, t=10.0, debug=True):
-    H = linspace(0.0, 10.0, N)
-    dxdt = lambda x : g - C2 * x
+    # also need to get clarity on what error is to be computed (abs_err, local/ global)
+    # and how the each implies in the question paper
 
-    y = zeros(N, dtype=float)
-
-    #euler's method: yn+1 = yn + h * f(tn, yn)
+    """
+    compute error here
+    """
+    return y
 
 def question_b(debug=False):
     pass
@@ -32,14 +40,15 @@ def plot_computed_error(N, error):
 
 
 if __name__ == "__main__":
-    from numpy import linspace, array, zeros
+    from numpy import (linspace, array, zeros)
+    from scipy import (integrate, special)
     import matplotlib.pyplot as plt
+    from math import (sqrt, tanh)
 
     N = [10, 100, 1000]
     for n in N:
         plot_computed_error(n, question_a(n))
-    question_b()
-
+    #question_b()
 else:
     from sys import exit
     exit("USAGE: python question1.py")
