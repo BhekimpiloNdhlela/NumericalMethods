ec9f4f8b5d3ea5c54ad272ccc10b8621026dc7b7
BhekiSibbs <28536165+BhekimpiloNdhlela@users.noreply.github.com>
2018-05-16 16:51:07 +0200

    Update question03.py

--- assignment_06/src/question03.py
+++ assignment_06/src/question03.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def adam_bashforth_two_step(f, h, n=100, t=[1.0, 2.0], w0=4.0):
+def adam_bashforth_two_step(f, n, t=[1.0, 2.0], w0=4.0):
     W    = zeros((int(n),), dtype=float)
     t    = linspace(t[0], t[1], int(n))
+    h    = 1 / float(n)
     W[0] = w0
-    W[1] = W[0] + h * f(1.0, W[0])    # w[1] evaluated by euler's method
+    W[1] = W[0] + h * f(t[1], W[0])    # w[1] evaluated by euler's method
 
     for i, j in zip(range(1, int(n - 1)), range(2, int(n))):
         W[i+1] = W[i] + (0.5*h)*(3*f(t[j], W[i]) - f(t[j-1], W[i-1]))
@@ -15,41 +16,40 @@ def adam_bashforth_two_step(f, h, n=100, t=[1.0, 2.0], w0=4.0):
     print "Actual Value: @ h = ", h, "\tW[-1] = ", W[-1]
     return W[-1]
 
-def adam_bashforth_mul_step(f, h, n=100, T=(1.0, 2.0), w0=4.0):
+def adam_bashforth_mul_step(f, n, t=(1.0, 2.0), w0=4.0):
     wn   = lambda w0, w1, t0, t1 : -4.0*w0 + 5.0*w1 + h*(4*f(t0, w0) + 2*f(t1, w1))
     W    = zeros((int(n),), dtype=float)
-    t    = linspace(T[0], T[1], int(n))
+    t    = linspace(t[0], t[1], int(n))
+    h    = 1 / float(n)
     W[0] = w0
     W[1] = W[0] + h * f(t[1], W[0])    # w[1] evaluated by euler's method
 
     for i, j in zip(range(1, int(n)), range(2, int(n))):
         W[i+1] = -4.0*W[i] + 5.0*W[i-1] + h*(4*f(t[j], W[i]) + 2*f(t[j-1], W[i-1]))
 
-    print t[j]
     # Update global Variables befor returning
     global MULTI_STEP_SOLUTIONS
     MULTI_STEP_SOLUTIONS.append(W)
-    #print "h = ", h, "W[-i]", W[-1]
+    print "Actual Value: @ h = ", h, "\tW[-1] = ", W[-1]
     return W[-1]
 
-def plot_solution_functions(W, t, method_used):
-    plt.title(method_used)
-    plt.ylabel("dy/dt = -t * y ")
+def plot_solution_function():
+    I =  lambda t: 4.0*exp(0.5*(1-((t)**2.0)))
+    x = linspace(1, 2, 200)
+    y = I(x)
+    plt.title("")
+    plt.ylabel("y = 4.0*exp(0.5*(1-((t)**2.0)))")
     plt.xlabel("time = t)")
     plt.xlim([1, 2])
-    plt.plot(t, W[0], 'k-', linewidth=3, label="h = 0.0100")
-    plt.plot(t, W[1], 'b-', linewidth=3, label="h = 0.0050")
-    plt.plot(t, W[2], 'c-', linewidth=3, label="h = 0.0025")
-    #plt.plot(t, W[3], 'r-', linewidth=3, label="h = 0.0000")
-    plt.legend(bbox_to_anchor=(.4, .4))
+    plt.plot(x, y, 'c-', linewidth=3)
     plt.show()
 
 def debug(abs_err_2, abs_err_m, debug_status=True):
     if debug_status == True:
         print("Absolute Errors:")
-        print("Adam Bashforth Two Steps")#\tAdam Bashforth Multi Steps")
+        print("Adam Bashforth Two Steps\tAdam Bashforth Multi Steps")
         for step_2, step_m in zip(abs_err_2, abs_err_m):
-            print("{:.20f}".format(step_2))# + "\t\t " + "{:.20f}".format(step_m))
+            print("{:.20f}".format(step_2) + "\t\t " + "{:.2f}".format(step_m))
 
 # Global Variables
 MULTI_STEP_SOLUTIONS = []
@@ -62,20 +62,17 @@ if __name__ == "__main__":
     f       = lambda t, y : -t * y
     I       =  4.0*exp(0.5*(1-((2)**2.0)))
     abs_err = lambda xc   : abs(xc - I)
-    H       = [0.01, 0.005, 0.0025]
-    N       = 100
+    N       = [100.0, 200.0, 400.0]
     print "I = ", I
+    two_step_abs_err   = [abs_err(adam_bashforth_two_step(f, n)) for n in N]
 
-    #print help(arange)
 
-    two_step_abs_err   = [abs_err(adam_bashforth_two_step(f, h, n=N)) for h in H]
-    multi_step_abs_err = [abs_err(adam_bashforth_mul_step(f, h, n=N)) for h in H]
-    debug(two_step_abs_err, multi_step_abs_err, debug_status=True)
-
-    #m_status0 = "Adam Bashforth Two Step Method"
-    #m_status1 = "Adam Bashforth Multi Step Method"
-    #plot_solution_functions(TWO_STEP_SOLUTIONS, linspace(1.0, 2.0, N), m_status0)
-    #plot_solution_functions(MULTI_STEP_SOLUTIONS, linspace(1, 2, N), m_status1)
+    print("\n")
+    multi_step_abs_err = [abs_err(adam_bashforth_mul_step(f, n)) for n in N]
+    for n, err in zip(N, two_step_abs_err):
+        print "@ N = ", n,"\t\t{:.10f}".format(err)
+    #debug(two_step_abs_err, multi_step_abs_err, debug_status=True)
+    plot_solution_function()
 else:
     from sys import exit
     exit("USAGE: question3.py")
