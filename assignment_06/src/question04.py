f6ca70347357fd480ed28faa3513fe45a6d2162b
Bhekimpilo <18998712@sun.ac.za>
2018-05-21 18:16:56 +0200

    add code for q4.py and the correspong img

--- assignment_06/src/question04.py
+++ assignment_06/src/question04.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
-
-def simpsons_rule_max_h():
-    pass
+def plot_function(x, y):
+    hor_function = lambda x : 1
+    plt.title("The function is less than 1 below \nthe horizontal black dashed line")
+    plt.xlabel("horizontal axis")
+    plt.ylabel("vertical axis")
+    plt.plot(x, y, 'r-', lw=5)
+    plt.plot(0, 1, 'mo', lw=5)
+    plt.axhline(y=1.0, xmin=-0.5, xmax=1.0, color='k', ls='--', lw=4)
+    plt.show()
 
 if __name__ == "__main__":
-    f = lambda t, y: -10 * y
-    f0 = 1
+    import matplotlib.pyplot as plt
+    from numpy import linspace
+    H = lambda h: 1 - 10*h + float(pow(-10 * h, 2))/2.0  + float(pow(-10 * h, 3))/6.0
+    X = linspace(-0.5, 1, 100)
+    Y = [H(x) for x in X]
+    i = 0
+    #for x, y in zip(X, Y):
+    #    if y == 1.0 and i ==0:
+    #        i += 1
+    #        print'(x, y) |=> (', x, y, ')'
+    plot_function(X, Y)
 else:
     from sys import exit
     exit("USAGE: pthon[2/3] question04.py")
